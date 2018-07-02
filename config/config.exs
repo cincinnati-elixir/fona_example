6aa665bcf99556e380a6d4f9427cce1d7fefc4c7
Tim Mecklem <timothy@mecklem.com>
2018-07-02 13:32:26 -0400

    Add nerves_init_gadget

--- config/config.exs
+++ config/config.exs
@@ -1,23 +1,16 @@
-# This file is responsible for configuring your application
-# and its dependencies with the aid of the Mix.Config module.
-#
-# This configuration file is loaded before any dependency and
-# is restricted to this project.
 use Mix.Config
 
-# Customize non-Elixir parts of the firmware.  See
-# https://hexdocs.pm/nerves/advanced-configuration.html for details.
+config :logger, backends: [RingLogger]
+
 config :nerves, :firmware, rootfs_overlay: "rootfs_overlay"
 
-# Use shoehorn to start the main application. See the shoehorn
-# docs for separating out critical OTP applications such as those
-# involved with firmware updates.
-config :shoehorn,
-  init: [:nerves_runtime],
-  app: Mix.Project.config()[:app]
+config :nerves_firmware_ssh,
+  authorized_keys: [
+    File.read!(Path.join(System.user_home!, ".ssh/id_rsa.pub"))
+  ]
 
-# Import target specific config. This must remain at the bottom
-# of this file so it overrides the configuration defined above.
-# Uncomment to use target specific configurations
+config :shoehorn,
+  init: [:nerves_runtime, :nerves_init_gadget],
+  app: Mix.Project.config()[:app]
 
 # import_config "#{Mix.Project.config[:target]}.exs"
